bd4d011fd5480cfc3fc326f42a3ab5905e3b699b
Gianmarco <gianmarcogeek@gmail.com>
2024-04-18 14:07:06 +0200

    first pixel

--- src/my_mlx_pixel_put.c
+++ src/my_mlx_pixel_put.c
@@ -6,18 +6,18 @@
 /*   By: gpuscedd <gpuscedd@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:53:24 by gpuscedd          #+#    #+#             */
-/*   Updated: 2024/04/18 13:55:11 by gpuscedd         ###   ########.fr       */
+/*   Updated: 2024/04/18 14:06:32 by gpuscedd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./include/fdf.h"
+#include "../include/fdf.h"
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 	int		offset;
 	
-	offset = (y * data->line_length + x * (data->bits_per_pixel / 8));
+	offset = (y * data->line_lenght + x * (data->bits_per_pixel / 8));
 	dst = data->addr + offset;
 	*(unsigned int*)dst = color;
 }
